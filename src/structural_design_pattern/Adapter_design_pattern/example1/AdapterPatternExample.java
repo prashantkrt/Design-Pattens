8b6e9478ce1a4e9a75baf273b1a8742c986f12f4
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-07-14 19:58:48 +0530

    Adapter Design Pattern

--- src/structural_design_pattern/Adapter_design_pattern/example1/AdapterPatternExample.java
+++ src/structural_design_pattern/Adapter_design_pattern/example1/AdapterPatternExample.java
@@ -2,6 +2,7 @@ package structural_design_pattern.Adapter_design_pattern.example1;
 
 public class AdapterPatternExample {
     public static void main(String[] args) {
+
         MyApplication application = new MyApplication();
         application.setPaymentType("UPI");
         application.setDetails("myapp@upi");
