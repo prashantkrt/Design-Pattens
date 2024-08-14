040ca53ee23d9114414143e37dadc893df826b28
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-08-14 22:45:06 +0530

    Proxy Design Pattern

---
+++ src/structural_design_pattern/Proxy_design_pattern/example2/InsurancePolicyImpl.java
@@ -0,0 +1,42 @@
+package structural_design_pattern.Proxy_design_pattern.example2;
+
+public class InsurancePolicyImpl implements InsurancePolicy {
+
+    private String name;
+    private String description;
+    private double price;
+
+    public InsurancePolicyImpl(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        loadingPolicyDetails(); // simulating loading policy details from db
+    }
+    
+    //when class object created automatically data is being loaded from a database
+    private void loadingPolicyDetails() {
+        //simulating loading policy details from a database
+        System.out.println("Loading data from database");
+        try {
+            Thread.sleep(1000); // simulate slow data loading
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Policy detail is loaded!!!");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPremium() {
+        return price;
+    }
+}
---
+++ src/structural_design_pattern/Proxy_design_pattern/example2/InsurancePolicyProxy.java
@@ -0,0 +1,45 @@
+package structural_design_pattern.Proxy_design_pattern.example2;
+
+// proxy class
+public class InsurancePolicyProxy implements InsurancePolicy {
+
+    private String name;
+    private String description;
+    private double amount;
+    private InsurancePolicy realInsurancePolicy;
+
+    public InsurancePolicyProxy(String name, String description, double amount) {
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    @Override
+    public String getName() {
+        if(realInsurancePolicy == null) {
+            loadRealPolicy();
+        }
+        return realInsurancePolicy.getName();
+    }
+
+    @Override
+    public String getDescription() {
+        if(realInsurancePolicy == null) {
+            loadRealPolicy();
+        }
+        return realInsurancePolicy.getDescription();
+    }
+
+    @Override
+    public double getPremium() {
+        if(realInsurancePolicy == null) {
+            loadRealPolicy();
+        }
+        return realInsurancePolicy.getPremium();
+    }
+
+    private void loadRealPolicy() {
+        System.out.println("Loading policy details from the insurance ");
+        realInsurancePolicy = new InsurancePolicyImpl(name, description, amount);
+    }
+}
